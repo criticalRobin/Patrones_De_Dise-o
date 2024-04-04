50f9bda34c7018e15686e17e8ccae7678fb98823
Matias Tite <matiastite1@gmail.com>
2024-04-03 22:32:30 -0500

    Implemented Student instance singleton

--- singleton/src/main/java/com/matias/tite/App.java
+++ singleton/src/main/java/com/matias/tite/App.java
@@ -4,10 +4,13 @@ package com.matias.tite;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Student studentA = Student.getSingletonInstance("Matias", "Tite", "FISEI");
+
+        System.out.println(studentA.toString());
+
+        Student studentB = Student.getSingletonInstance("Juan", "Perez", "FISEI");
+        System.out.println(studentB.toString());
     }
 }
