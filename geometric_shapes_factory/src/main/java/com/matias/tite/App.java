857b3df10ed25339c1c4f21391b2d5f24e8ee496
Matias Tite <matiastite1@gmail.com>
2024-04-10 11:03:10 -0500

    Nuevo proyecto aplicando factory

---
+++ geometric_shapes_factory/src/main/java/com/matias/tite/App.java
@@ -0,0 +1,13 @@
+package com.matias.tite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ geometric_shapes_factory/src/main/java/com/matias/tite/factory/shapes/GeometricShape.java
@@ -0,0 +1,7 @@
+package com.matias.tite.factory.shapes;
+
+public interface GeometricShape {
+    public double getArea();
+
+    public double getPerimeter();
+}
